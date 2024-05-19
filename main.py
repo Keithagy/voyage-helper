4cf537029cb092db1ad8cc0b044a832e9363464a
Keith Ang <keithang@Keiths-MacBook-Pro.local>
2024-05-20 05:29:21 +0800

    fix: erroneous argument for message send

--- main.py
+++ main.py
@@ -74,8 +74,8 @@ def start(
 
         if len(group_ids_accessible_for_user) == 1:
             await message.reply_text(
-                f"You're part of just the one Astralship telegram channel ({( await context.bot.get_chat(group_ids_accessible_for_user[0]) ).effective_name}), so I'll take it that you're doing energy accounting for that.",
-                "Whenever you're ready, tell me via voice or text what you did today and how much time it took.",
+                f"You're part of just the one Astralship telegram channel ({( await context.bot.get_chat(group_ids_accessible_for_user[0]) ).effective_name}), so I'll take it that you're doing energy accounting for that."
+                "Whenever you're ready, tell me via voice or text what you did today and how much time it took."
             )
             context_chat_data = context.chat_data
             if context_chat_data is None:
